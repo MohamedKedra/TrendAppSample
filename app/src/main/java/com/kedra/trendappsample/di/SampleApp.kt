c4fdebb794ee392b12acecbfd2e7e0433908e373
Mohamed Ali Kedra <mohamed.a.kedra@gmail.com>
2021-11-16 09:52:25 +0200

    Revert "Implement repo api"

--- app/src/main/java/com/kedra/trendappsample/di/SampleApp.kt
+++
@@ -1,8 +0,0 @@
-package com.kedra.trendappsample.di
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class SampleApp : Application() {
-}
--- app/src/main/java/com/kedra/trendappsample/ui/main/MainFragment.kt
+++ app/src/main/java/com/kedra/trendappsample/ui/main/MainFragment.kt
@@ -1,86 +1,31 @@
-              package com.kedra.trendappsample.ui.main
+package com.kedra.trendappsample.ui.main
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.viewModels
-import android.widget.ExpandableListAdapter
-import androidx.core.view.isVisible
-import com.kedra.trendappsample.app.DataState
-import com.kedra.trendappsample.databinding.MainFragmentBinding
-import dagger.hilt.android.AndroidEntryPoint
+import com.kedra.trendappsample.R
 
-@AndroidEntryPoint
 class MainFragment : Fragment() {
 
     companion object {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var binding: MainFragmentBinding
-
-    private lateinit var adapter: CustomExpandableListAdapter
-
-    private val viewModel by viewModels<MainViewModel>()
+    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        binding = MainFragmentBinding.inflate(inflater, container, false)
-        return binding.root
+        return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        with(binding) {
-            lvItems.isVisible = false
-            shimmerLayout.startShimmerAnimation()
-            Thread.sleep(5000)
-            adapter = CustomExpandableListAdapter(
-
-                requireContext(),
-
-                )
-            this.lvItems.setAdapter(adapter)
-            lvItems.isVisible = true
-            shimmerLayout.stopShimmerAnimation()
-        }
+        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        initObserveData()
-    }
-
-    private fun initObserveData() {
-        viewModel.refreshHomeList().observe(viewLifecycleOwner, {
-            when (it.getStatus()) {
-
-                DataState.DataStatus.LOADING -> {
-                }
-
-                DataState.DataStatus.SUCCESS -> {
-                    Log.d("res",it.getData().toString())
-                    it.getData()?.let { list ->
-                        adapter.addList(list)
-                    }
-
-                }
-
-                DataState.DataStatus.ERROR -> {
-                    Log.d("res",it.getError().toString())
-                }
-
-                DataState.DataStatus.NO_INTERNET -> {
-
-                }
-
-            }
-        })
-    }
 }
